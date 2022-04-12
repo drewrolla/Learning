bd8c6f5fab16c24ce66293a6e7bf31c43f1b118e
Andrew <60520464+drewrolla@users.noreply.github.com>
2022-04-12 05:48:20 -0400

    Add files via upload

---
+++ dogyears.js
@@ -0,0 +1,18 @@
+var myAge = 24;
+
+var earlyYears = 2;
+
+earlyYears *= 10.5;
+
+var laterYears = myAge - 2;
+
+laterYears *= 4;
+
+// console.log(earlyYears);
+// console.log(laterYears);
+
+var myAgeInDogYears = earlyYears + laterYears;
+
+var myName = "Drewrolla".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
