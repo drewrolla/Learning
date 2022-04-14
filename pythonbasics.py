74c892a89ceb0b01023db7fb225cdfb6e15784ad
Andrew <60520464+drewrolla@users.noreply.github.com>
2022-04-14 04:45:22 -0400

    Add files via upload

---
+++ pythonbasics.py
@@ -0,0 +1,117 @@
+#comments in Python are different from comments in other programming languages and it's pretty neat to learn it all
+
+
+#self-explanatory
+print("Hello world!");
+
+
+#use of different quotes for strings
+print("drewrolla");
+
+print('drewrolla');
+
+
+#learning variables
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Pizza sandwich"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Sushi and fries"
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+
+#fixing mismatched quotes
+print('This message has mismatched quote marks!')
+print("Abracadabra")
+
+
+#numbers
+# Define the release and runtime integer variables below:
+release_year = 2012;
+runtime = 120;
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = 6.9;
+
+#calculations
+print(25 * 68 + 13 / 28);
+
+#changing numbers
+quilt_width = 8;
+#original quilt_length = 12;
+quilt_length = 8;
+
+#original print(8 * 12);
+
+print(8*8);
+
+#exponents
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2);
+# 7x7 quilt
+print(7 ** 2);
+# 8x8 quilt
+print(8 ** 2);
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print (6 ** 4);
+
+#modulo
+my_team = 27 % 4;
+print(my_team);
+
+#optional modulo
+print(26%4);
+print(28%4);
+
+#concatenation
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6;
+
+#print(message)
+print(message);
+
+
+#plus equals
+total_price = 0
+
+new_sneakers = 50.00
+
+total_price += new_sneakers
+
+nice_sweater = 39.00
+fun_books = 20.00
+# Update total_price here:
+total_price += nice_sweater;
+total_price += fun_books;
+
+print("The total price is", total_price)
+
+
+#multilines
+# Assign the string here
+to_you = """Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you? """
+
+print(to_you)
