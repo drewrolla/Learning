ab0ba82b28346bb4081cbc3b5ed35874ceb9c7d2
Andrew <60520464+drewrolla@users.noreply.github.com>
2022-04-14 05:00:24 -0400

    Add files via upload

---
+++ introtofunctionsandparameters.js
@@ -0,0 +1,24 @@
+//functions
+function getReminder() {
+  console.log('Water the plants.');
+}
+
+function greetInSpanish() {
+  console.log('Buenas Tardes.');
+}
+
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
+
+
+//parameters and arguments
+function sayThanks(name) {
+  console.log('Thank you for your purchase ' + name +  '! We appreciate your business.');
+}
+
+sayThanks('Cole');
