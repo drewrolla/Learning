e9989c9bcc26cf7b226cbce4fbb143151df2364e
Andrew <60520464+drewrolla@users.noreply.github.com>
2022-04-15 04:20:10 -0400

    Add files via upload

---
+++ beginnings2.rb
@@ -0,0 +1,36 @@
+#'.length' method
+puts "drewrolla".length
+
+#'.reverse' method
+puts "drewrolla".reverse
+
+#'.upcase' and '.downcase'
+puts "drewrolla".upcase
+
+puts "DREWROLLA".downcase
+
+
+#multi-line comments lol - how meta, using a single line comment for multi-line
+=begin
+So this is what a multi-line comment is?
+Pretty cool.
+I like trains. - IYKYK
+=end
+
+
+#variables
+name = "drewrolla"
+my_name = "drewrolla"
+my_age = 24
+
+
+#more math
+puts sum = 13 + 379
+puts product = 923 * 15
+puts quotient = 13209 / 17
+
+
+#more strings and string methods
+puts name = "Drewrolla".downcase
+puts name.downcase.reverse
+puts name.downcase.reverse.upcase
