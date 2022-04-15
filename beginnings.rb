0ec71a245292bfa17ebb83c15d3c23febe6dff20
Andrew <60520464+drewrolla@users.noreply.github.com>
2022-04-15 04:07:37 -0400

    Add files via upload

---
+++ beginnings.rb
@@ -0,0 +1,43 @@
+#learning ruby
+#data types: numbers, strings, booleans
+my_num = 25    # Add your code here!
+
+my_boolean = true    # And here!
+
+my_string = "Ruby"    # Also here.
+
+puts my_num
+puts my_boolean
+puts my_string
+
+#variables
+my_num = 100
+
+# Write code above this line!
+
+puts my_num
+
+
+#math
+exponent = 3 ** 2
+modulo = 9 % 2
+addition = 3 + 3
+subtraction = 3 - 3
+multiplication = 9 * 9
+division = 100 / 10
+
+puts exponent
+puts modulo
+puts addition
+puts subtraction
+puts multiplication
+puts division
+
+#print and puts
+#does not require semicolons
+print "I like pizza."
+puts "I really like pizza."
+print "What is your favorite type of pizza?"
+puts "Anchovy."
+print "Gross."
+
