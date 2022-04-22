26cdf5610edd452113bc235931466ed2179430f8
Andrew <60520464+drewrolla@users.noreply.github.com>
2022-04-22 06:55:46 -0400

    Add files via upload

---
+++ controlflows2.rb
@@ -0,0 +1,26 @@
+#equals not equals
+is_true = 2 != 3
+
+is_false = 2 == 3
+
+#less than greater than
+test_1 = 17 > 16
+
+test_2 = 21 < 30
+
+test_3 = 9 >= 9
+
+test_4 = -11 <= 4
+
+#more practice
+# test_1 = 77 != 77
+test_1 = false
+
+# test_2 = -4 <= -4
+test_2 = true
+
+# test_3 = -44 < -33
+test_3 = true
+
+# test_4 = 100 == 1000
+test_4 = false
