09237892243011b82ecdb841af1eceaec5ddaff1
Andrew <60520464+drewrolla@users.noreply.github.com>
2022-04-30 07:35:01 -0400

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,20 @@
+//calling items in array
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]);
+
+//changing items in array
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+condiments[0] = 'Mayo';
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+console.log(condiments);
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
